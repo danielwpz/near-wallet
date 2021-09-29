0e81d187803d403b77291f3d565414d7e1363966
marcinbodnar <me@marcinbodnar.com>
2021-09-29 02:04:03 +0200

    Combine new availableAccounts reducer (#2168)

--- packages/frontend/src/redux/combineReducers.js
+++ packages/frontend/src/redux/combineReducers.js
@@ -4,12 +4,12 @@ import { combineReducers } from 'redux';
 
 import account from './reducers/account';
 import allAccounts from './reducers/allAccounts';
-import availableAccounts from './reducers/available-accounts';
 import ledger from './reducers/ledger';
 import recoveryMethods from './reducers/recoveryMethods';
 import sign from './reducers/sign';
 import staking from './reducers/staking';
 import status from './reducers/status';
+import availableAccounts from './slices/availableAccounts';
 import createFromImplicitSlice from './slices/createFromImplicit';
 import flowLimitationSlice from './slices/flowLimitation';
 import linkdropSlice from './slices/linkdrop';
@@ -21,7 +21,6 @@ import transactionsSlice from './slices/transactions';
 export default (history) => combineReducers({
     localize: localizeReducer,
     allAccounts,
-    availableAccounts,
     account,
     sign,
     recoveryMethods,
@@ -35,5 +34,6 @@ export default (history) => combineReducers({
     [tokensSlice.name]: tokensSlice.reducer,
     [flowLimitationSlice.name]: flowLimitationSlice.reducer,
     [createFromImplicitSlice.name]: createFromImplicitSlice.reducer,
+    availableAccounts,
     router: connectRouter(history)
 });
